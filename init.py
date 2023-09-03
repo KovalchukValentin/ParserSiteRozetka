c4fc77d7407c11c5a73f0a4d4401313b22ea4803
KovalchukValentin <kovalchukvalentine@gmail.com>
2023-09-03 17:59:27 +0300

    added function for get all values from first pages in int list

--- init.py
+++ init.py
@@ -3,10 +3,10 @@ from core import Parcer
 
 def view(links):
     for link in links:
-        print(link.text)
+        print(link)
 
 
 if __name__ == "__main__":
     url = 'https://rozetka.com.ua/ua/mobile-phones/c80003/'
     parser = Parcer(url=url)
-    [view(result) for result in parser.get_all_link_tiles_from_first_pages(2)]
+    [view(result) for result in parser.get_all_values_from_first_pages_in_int_list(2)]
