0abcabbf856bd5e53d827e94fbd0e0fef51deacf
KovalchukValentin <kovalchukvalentine@gmail.com>
2023-09-03 16:23:44 +0300

    the logic of searching for product links has been changed

--- core.py
+++ core.py
@@ -5,7 +5,7 @@ from bs4 import BeautifulSoup
 class Parcer:
     def __init__(self, url):
         self.response = self.get_work_response(url)
-        self.links = self.get_all_links(self.response)
+        self.links = self.get_all_links()
         self.max_page = self.get_max_page(links=self.links)
 
     def get_work_response(self, url) -> requests.Response:
@@ -16,8 +16,8 @@ class Parcer:
             print('Failed to retrieve the webpage. Status code:', response.status_code)
             exit()
 
-    def get_all_links(self, response) -> list:
-        html_content = response.text
+    def get_all_links(self) -> list:
+        html_content = self.response.text
         soup = BeautifulSoup(html_content, 'html.parser')
         return soup.find_all('a')
 
@@ -35,22 +35,28 @@ class Parcer:
         return not link.text or not link.get('href')
 
     def get_all_models_of_mobile(self) -> list:
-        return self._get_all_models_of_mobile(self.links)
+        return self._get_all_a_with_class(class_="goods-tile__heading ng-star-inserted")
 
-    def _get_all_models_of_mobile(self, links) -> list:
-        models_of_mobile = []
-        flag_top = 2
-        for link in links:
-            if self.is_bed_link(link):
-                continue
-            if link.get('href')[0] == '/' or link.text[1].isnumeric():
-                continue
-            if flag_top:
-                if flag_top == 1:
-                    flag_top -= 1
-                elif link.get('href') == 'https://rozetka.com.ua/ua/':
-                    flag_top -= 1
-                continue
-            if link.get('href') == 'https://rozetka.com.ua/ua/contacts/':
-                return models_of_mobile
-            models_of_mobile.append(link)
+    # def _get_all_models_of_mobile(self, links) -> list:
+    #     models_of_mobile = []
+    #     flag_top = 2
+    #     for link in links:
+    #         if self.is_bed_link(link):
+    #             continue
+    #         if link.get('href')[0] == '/' or link.text[1].isnumeric():
+    #             continue
+    #         if flag_top:
+    #             if flag_top == 1:
+    #                 flag_top -= 1
+    #             elif link.get('href') == 'https://rozetka.com.ua/ua/':
+    #                 flag_top -= 1
+    #             continue
+    #         if link.get('href') == 'https://rozetka.com.ua/ua/contacts/':
+    #             return models_of_mobile
+    #         models_of_mobile.append(link)
+
+    def _get_all_a_with_class(self, class_):
+        html_content = self.response.text
+        soup = BeautifulSoup(html_content, 'html.parser')
+        return soup.find_all("a", class_=class_)
+
